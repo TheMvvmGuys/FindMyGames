9564dfb1df366e030eb010fa1eeb75fb320e5231
TheMulti0 <multi@codeprecise.com>
2019-01-15 20:17:23 +0200

    This is getting better

---
+++ GameTracker/GameFolder.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GameTracker
+{
+    public class GameFolder
+    {
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     The full name with all of the parents we know about (if any)
+        /// </summary>
+        public string FullKnownName { private get; set; }
+
+        private IEnumerable<string> Parents => FullKnownName
+           .Split('/')
+           .Reverse()
+           .Skip(1);
+
+        public GameFolder(string folderPath)
+        {
+            Name = folderPath.Split('/')
+               .LastOrDefault();
+            FullKnownName = folderPath;
+        }
+
+        public bool IsDirectoryInfoParentEqual(DirectoryInfo directory)
+        {
+            var index = 0;
+            DirectoryInfo currentDirectory = directory;
+            while (GetParentsEquality(currentDirectory))
+            {
+                //Go up in the tree
+                currentDirectory = currentDirectory?.Parent;
+                index++;
+            }
+
+            return index > 0;
+        }
+
+        private bool GetParentsEquality(DirectoryInfo d)
+        {
+            if (d?.Parent == null)
+            {
+                return false;
+            }
+            return d.Parent?.Name == Parents.First();
+        }
+    }
+}
---
+++ GameTracker/GameTracker.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameTracker
+{
+    internal class GameTracker
+    {
+        private readonly IList<GameFolder> _appropriateGameFolders = new List<GameFolder>();
+
+        public IEnumerable<GameFolder> TrackGamesAllDrives()
+        {
+            //this will move
+            string wantedPath = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
+            IEnumerable<string> gameFoldersDatabase = File.ReadLines($"{wantedPath}\\db.txt");
+            foreach (string folderPath in gameFoldersDatabase)
+            {
+                _appropriateGameFolders.Add(new GameFolder(folderPath));
+            }
+            List<GameFolder> gameFolders = new List<GameFolder>();
+
+            DriveInfo.GetDrives()
+               .AsParallel()
+               .ForAll(
+                    drive =>
+                    {
+                        IEnumerable<GameFolder> driveGameFolders = EnumerateDirectoryChildren(drive.RootDirectory);
+                        gameFolders.AddRange(driveGameFolders);
+                    });
+
+            return gameFolders;
+        }
+
+        private IEnumerable<GameFolder> EnumerateDirectoryChildren(DirectoryInfo di)
+        {
+            ConcurrentBag<GameFolder> gameFolders = new ConcurrentBag<GameFolder>();
+            Parallel.ForEach(
+                di.EnumerateDirectories(),
+                (folder =>
+                {
+                    if (CheckDirectory(folder))
+                    {
+                        gameFolders.Add(new GameFolder(folder.FullName));
+                    }
+                }));
+            return gameFolders;
+        }
+
+        private bool CheckDirectory(DirectoryInfo directory)
+        {
+            if (_appropriateGameFolders.Any(folder => folder.Name == directory.Name &&
+                                           folder.IsDirectoryInfoParentEqual(directory)))
+            {
+                return true;
+            }
+
+            try
+            {
+                if (directory.EnumerateDirectories()
+                   .Any())
+                {
+                    EnumerateDirectoryChildren(directory);
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            return false;
+        }
+    }
+}

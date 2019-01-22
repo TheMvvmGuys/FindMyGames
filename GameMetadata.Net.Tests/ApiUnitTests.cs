c186220b6136336e2fb74e82c5644d6a92664b1f
TheMulti0 <multi@codeprecise.com>
2019-01-22 10:21:57 +0200

    Fixed this strange compilation error

--- GameMetadata.Net.Tests/ApiUnitTests.cs
+++ GameMetadata.Net.Tests/ApiUnitTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GameMetadata.Net;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TheMvvmGuys.GameMetadata.Net.Tests
--- GameMetadata.Net/MetadataClient.cs
+++ GameMetadata.Net/MetadataClient.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using GameMetadata.Net;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
